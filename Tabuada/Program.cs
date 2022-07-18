69d311a81fe1beb3d3ffa2f54a6259fd43dd54df
Samira Pessoal <sixoobecan@gmail.com>
2022-07-18 10:16:48 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Qual tabuada: ");
+            int tab = Convert.ToInt32(Console.ReadLine());
+            
+         
+               for (int b = 2; b < 10; b++)
+                {
+                    Console.WriteLine(tab + "x" + b + "=" + tab * b);
+                }
+               Console.ReadLine();
+        } 
+
+
+
+
+
+        
+    }
+}
